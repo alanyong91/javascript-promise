b244ff2d0a38cac7e4fb14abc6f9f9a5a4584466
Alan Yong <lun91yong@gmail.com>
2021-05-31 12:21:48 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const promiseResolve = (value, time, throwError = false) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (!throwError) {
+        console.log(value);
+        resolve(value);
+      } else {
+        reject(new Error("something goes wrong"));
+      }
+    }, time);
+  });
+};
+
+const testPromise = () => {
+  return Promise.all([
+    promiseResolve(1, 2000),
+    promiseResolve(2, 1000),
+    promiseResolve(3, 7000, true),
+    promiseResolve(4, 3000)
+  ])
+    .then(function (values) {
+      console.log(`values: ${values}`);
+    })
+    .catch((error) => {
+      console.error(`throwing error: ${error.message}`);
+    });
+};
+
+testPromise();
