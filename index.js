78f745182f674bb41119af6abe690d7e0d52eca4
Alan Yong <lun91yong@gmail.com>
2021-05-31 12:28:48 +0800

    run index.js - install nodemon - remove src folder

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const testPromise = () => {
     promiseResolve(1, 2000),
     promiseResolve(2, 1000),
     promiseResolve(3, 7000, true),
-    promiseResolve(4, 3000)
+    promiseResolve(4, 3000),
+    promiseResolve(5, 3000)
   ])
     .then(function (values) {
       console.log(`values: ${values}`);
